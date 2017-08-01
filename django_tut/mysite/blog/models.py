7576c00972bf0df087f8c0c010ac3d39c5669aac
TheCoderNextdoor <a.banerjee6762@gmail.com>
2017-08-01 19:10:53 +0530

    Add files via upload

---
+++ django_tut/mysite/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=140)
+    body = models.TextField()
+    date = models.DateTimeField()
+    mood = models.TextField(default='')
+    
+    def __str__(self):
+      return self.title
